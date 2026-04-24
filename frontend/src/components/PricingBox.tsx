d2cfc76b09424d57d53835abcba8763e1c830cbf
varinder <varinder_gupta82@yahoo.com>
2026-04-24 12:26:14 +0300

    Added react typescript components

--- frontend/src/components/PricingBox.tsx
+++ frontend/src/components/PricingBox.tsx
@@ -1,35 +1,35 @@
 function PricingBox() {
   return (
-    <div class="pricing-container">
-      <div class="pricing-plan">
-        <div class="plan-title">Basic</div>
-        <div class="plan-price">$9.99/month</div>
-        <ul class="plan-features">
+    <div className="pricing-container">
+      <div className="pricing-plan">
+        <div className="plan-title">Basic</div>
+        <div className="plan-price">$9.99/month</div>
+        <ul className="plan-features">
           <li>✅ 10GB Storage</li>
           <li>✅ 1 User</li>
           <li>🚫 Support</li>
         </ul>
-        <button class="plan-button">Sign Up</button>
+        <button className="plan-button">Sign Up</button>
       </div>
-      <div class="pricing-plan">
-        <div class="plan-title">Standard</div>
-        <div class="plan-price">$19.99/month</div>
-        <ul class="plan-features">
+      <div className="pricing-plan">
+        <div className="plan-title">Standard</div>
+        <div className="plan-price">$19.99/month</div>
+        <ul className="plan-features">
           <li>✅ 50GB Storage</li>
           <li>✅ 5 Users</li>
           <li>✅ Phone &amp; Email Support</li>
         </ul>
-        <button class="plan-button">Sign Up</button>
+        <button className="plan-button">Sign Up</button>
       </div>
-      <div class="pricing-plan">
-        <div class="plan-title">Premium</div>
-        <div class="plan-price">$49.99/month</div>
-        <ul class="plan-features">
+      <div className="pricing-plan">
+        <div className="plan-title">Premium</div>
+        <div className="plan-price">$49.99/month</div>
+        <ul className="plan-features">
           <li>✅ 100GB Storage</li>
           <li>✅ 10 Users</li>
           <li>✅ 24/7 Support</li>
         </ul>
-        <button class="plan-button">Sign Up</button>
+        <button className="plan-button">Sign Up</button>
       </div>
     </div>
   );
