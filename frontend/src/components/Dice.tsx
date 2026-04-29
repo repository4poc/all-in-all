a4b17d5315f6c2db937c13d5e7ba7f41104eca9f
varinder <varinder_gupta82@yahoo.com>
2026-04-29 17:30:09 +0300

    added frontend workflow

--- frontend/src/components/Dice.tsx
+++ frontend/src/components/Dice.tsx
@@ -1,38 +1,35 @@
+import { useState } from "react";
+
 function Dice() {
+  const [result, setResult] = useState("");
+
   const handleClick = () => {
     var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
-
     var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
-
     var randomImageSource = "/assets/dices/" + randomDiceImage; //images/dice1.png - images/dice6.png
-
     var image1 = document.querySelectorAll("#dice1")[0];
-
     image1.setAttribute("src", randomImageSource);
 
     var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-
     var randomDiceImage2 = "dice" + randomNumber2 + ".png";
-
     var randomImageSource2 = "/assets/dices/" + randomDiceImage2;
-
     var image2 = document.querySelectorAll("#dice2")[0];
-
     image2.setAttribute("src", randomImageSource2);
 
     //If player 1 wins
     if (randomNumber1 > randomNumber2) {
-      document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+      setResult("🚩 Player 1 Wins!");
     } else if (randomNumber2 > randomNumber1) {
-      document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+      setResult("Player 2 Wins! 🚩");
     } else {
-      document.querySelector("h1").innerHTML = "Draw!";
+      setResult("Draw!");
     }
   };
 
   return (
     <div id="dice-container" className="container">
       <h1>Refresh Me</h1>
+      <div id="result">{result}</div>
 
       <div className="dice">
         <p>Player 1</p>
