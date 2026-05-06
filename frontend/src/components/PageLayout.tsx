9f3990998ae69126000570768635327582a1fff2
varinder <varinder_gupta82@yahoo.com>
2026-05-06 23:09:55 +0300

    Fix: code quality frontend

--- frontend/src/components/PageLayout.tsx
+++ frontend/src/components/PageLayout.tsx
@@ -19,7 +19,7 @@ interface PageLayoutProps {
 const ProfileContent = () => {
   const { instance, accounts } = useMsal();
 
-  const [graphData, setGraphData] = useState<any>(null);
+  const [graphData, setGraphData] = useState<unknown>(null);
 
   function RequestProfileData() {
     // Silently acquires an access token which is then attached to a request for MS Graph data
