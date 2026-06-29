5b266340b42324c7db03d64519350b4be87a5988
varinder <varinder_gupta82@yahoo.com>
2026-06-29 18:32:46 +0300

    Front-end OAuth disable

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,13 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-
+/*
 import { PublicClientApplication } from "@azure/msal-browser";
 import { MsalProvider } from "@azure/msal-react";
 import { msalConfig } from "./authConfig";
 
 const msalInstance = new PublicClientApplication(msalConfig);
-/*
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <MsalProvider instance={msalInstance}>
