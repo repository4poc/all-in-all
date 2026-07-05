81e08af34b2d0ae5df435b724af63fe7334897a6
varinder <varinder_gupta82@yahoo.com>
2026-07-05 19:58:05 +0300

    AWS learning

--- infrastructure_cdk/infra_project/lib/infra_project-stack.ts
+++ infrastructure_cdk/infra_project/lib/infra_project-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from "aws-cdk-lib/core";
 import { Construct } from "constructs";
 import * as sqs from "aws-cdk-lib/aws-sqs";
+import { Fn } from "aws-cdk-lib/core";
 
 export class InfraProjectStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -30,5 +31,15 @@ export class InfraProjectStack extends cdk.Stack {
       description:
         "The duration in seconds for the SQS queue visibility timeout",
     });
+  }
+
+  // import { Fn } from "aws-cdk-lib/core";
+  private getStackName() {
+    const stackId = this.stackId;
+    // Stack ID
+    // arn:aws:cloudformation:eu-north-1:127112147701:stack/InfraProjectStack/c0c271d0-7888-11f1-80c8-0e9dba8833f7
+    const stackFCDN = Fn.select(5, Fn.split(":", stackId));
+    // stack/InfraProjectStack/c0c271d0-7888-11f1-80c8-0e9dba8833f7
+    const stackName = Fn.select(1, Fn.split("/", stackFCDN));
   }
 }
