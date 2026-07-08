07d3194279376161f469d12e0af17e7c1db95d16
varinder <varinder_gupta82@yahoo.com>
2026-07-08 04:40:57 +0300

    AWS Learning

--- infrastructure_cdk/infra_project/lib/aws-ci-cd-stack.ts
+++ infrastructure_cdk/infra_project/lib/aws-ci-cd-stack.ts
@@ -6,16 +6,18 @@ import {
 } from "aws-cdk-lib/pipelines";
 import { Construct } from "constructs";
 
-// Define the AwsCICDStack class that extends cdk.Stack
 export class AwsCICDStack extends cdk.Stack {
-  // Define the constructor for the AwsCICDStack class
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     new CodePipeline(this, "MyPipeline", {
       pipelineName: "MyPipeline",
       synth: new ShellStep("Synth", {
-        input: CodePipelineSource.gitHub("repository4poc/all-in-all", "main"),
+        input: CodePipelineSource.gitHub("repository4poc/all-in-all", "main", {
+          authentication: cdk.SecretValue.secretsManager(
+            "github_token_aws_cdk",
+          ),
+        }),
         commands: ["npm ci", "npm run build", "npx cdk synth"],
         primaryOutputDirectory: "infrastructure_cdk/infra_project/cdk.out",
       }),
