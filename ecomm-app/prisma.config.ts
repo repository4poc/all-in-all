24f6690a2a4776fd208a14dd6f2f2f9e38bae2f9
varinder <varinder_gupta82@yahoo.com>
2026-09-14 01:24:38 +0300

    Implemented Signout

--- ecomm-app/prisma.config.ts
+++ ecomm-app/prisma.config.ts
@@ -2,14 +2,3 @@
 // npm install --save-dev prisma dotenv
 import 'dotenv/config';
 import { defineConfig } from 'prisma/config';
-
-export default defineConfig({
-  schema: 'prisma/schema.prisma',
-  migrations: {
-    path: 'prisma/migrations',
-  },
-  engine: 'classic',
-  datasource: {
-    url: env('DATABASE_URL'),
-  },
-});
