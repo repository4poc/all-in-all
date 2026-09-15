f8f04ce271cd2b29dd748d82091382087f1d901c
varinder <varinder_gupta82@yahoo.com>
2026-09-16 00:29:52 +0300

    Fix: issues

--- ecomm-app/app/admin/layout.tsx
+++ ecomm-app/app/admin/layout.tsx
@@ -12,10 +12,11 @@ export default function DashboardLayout({
       <h2 className='text-2xl pl-8'>Dashboard</h2>
       <Separator className='mt-2' />
       <div className='mt-12 grid grid-cols-12'>
-        <div className='lg:col-span-2'>
+        <div className='lg:col-span-2 col-span-4'>
           <Sidebar />
         </div>
-        <div className='lg:col-span-10'>{children}</div>
+
+        <div className='lg:col-span-10 col-span-8'>{children}</div>
       </div>
     </>
   );
