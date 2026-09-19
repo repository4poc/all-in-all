138196bb3fcedf7ce21293901c29d837bc88e547
varinder <varinder_gupta82@yahoo.com>
2026-09-19 23:32:23 +0300

    Image rendering issue

--- ecomm-app/app/tours/[id]/page.tsx
+++
@@ -1,7 +0,0 @@
-const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
-  const { id } = await params;
-
-  return <div>ID {id}</div>;
-};
-
-export default Page;
--- ecomm-app/app/tours/error.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-const Error = ({ error }: { error: Error }) => {
-  console.log(error);
-  return <div>error</div>;
-};
-
-export default Error;
